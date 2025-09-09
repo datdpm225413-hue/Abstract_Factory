f3267fe233132c9f3a5ce181486ebf74ab8fde07
datdpm225413-hue <dat_dpm225413@student.agu.edu.vn>
2025-09-09 11:04:34 +0700

    Add files via upload

---
+++ DPM224513_NguyenTienDat/DPM224513_NguyenTienDat_Read01_Abstract Factory/Herbivore.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM224513_NguyenTienDat_Read01_Abstract_Factory
+{
+    abstract class Herbivore { }
+}
---
+++ DPM224513_NguyenTienDat/DPM224513_NguyenTienDat_Read01_Abstract Factory/MainApp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM224513_NguyenTienDat_Read01_Abstract_Factory
+{
+    class MainApp
+    {
+        public static void Main()
+        {
+            // Tạo và chạy thế giới động vật Châu Phi
+            ContinentFactory africa = new AfricaFactory();
+            AnimalWorld world = new AnimalWorld(africa);
+            world.RunFoodChain();
+
+            // Tạo và chạy thế giới động vật Châu Mỹ
+            ContinentFactory america = new AmericaFactory();
+            world = new AnimalWorld(america);
+            world.RunFoodChain();
+
+            Console.ReadKey();
+        }
+    }
+}
